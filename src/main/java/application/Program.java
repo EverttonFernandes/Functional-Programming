bb95dfa8520d3661ee1a1e9ffe271dfa324c62b3
EverttonFernandes <evertonoliveirafernandes@outlook.com>
2021-09-09 00:04:58 -0300

    Using object comparator with anonymous class

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -3,7 +3,9 @@ package application;
 import entities.Product;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 
 public class Program {
     public static void main(String[] args) {
@@ -13,7 +15,14 @@ public class Program {
         productList.add(new Product("IPhone 12", 7000.00));
         productList.add(new Product("Playstation 5", 5500.00));
 
-        productList.sort(new MyComparator());
+        Comparator<Product> comparator = new Comparator<Product>() {
+            @Override
+            public int compare(Product p1, Product p2) {
+                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+            }
+        };
+
+        productList.sort(comparator);
 
         for (Product product : productList) {
             System.out.println(product);
